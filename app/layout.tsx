2ef939eb9eee8d0b8f7d588b5924a710e56c9fb4
SundaramUpadhyay <sundaramupadhyay2002@gmail.com>
2026-05-07 11:45:09 +0530

    Add Google Analytics tag (GA-ID: G-Y986EVKRH8) to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import LanguageSync from '@/components/language-sync'
@@ -20,6 +21,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y986EVKRH8"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Y986EVKRH8');
+            `,
+          }}
+        />
+      </head>
       <body className={`font-sans antialiased`}>
         <Providers>
           <LanguageSync />
